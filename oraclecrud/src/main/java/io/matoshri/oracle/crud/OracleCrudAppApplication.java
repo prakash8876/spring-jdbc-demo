a0db376ba3d0e03f11bf0b682f9b9f6caa40c122
prakash8876 <42605640+prakash8876@users.noreply.github.com>
2022-08-08 18:12:09 +0530

    added oracle crud app

---
+++ oraclecrud/src/main/java/io/matoshri/oracle/crud/OracleCrudAppApplication.java
@@ -0,0 +1,13 @@
+package io.matoshri.oracle.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleCrudAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleCrudAppApplication.class, args);
+	}
+
+}
