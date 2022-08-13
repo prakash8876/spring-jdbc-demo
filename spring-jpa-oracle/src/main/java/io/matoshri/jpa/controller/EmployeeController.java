b0b27148d40fec31376b53bbc82dc820639e0a23
prakash8876 <42605640+prakash8876@users.noreply.github.com>
2022-08-13 23:00:43 +0530

    EmployeeController added

--- spring-jpa-oracle/src/main/java/io/matoshri/jpa/controller/EmployeeController.java
+++ spring-jpa-oracle/src/main/java/io/matoshri/jpa/controller/EmployeeController.java
@@ -1,5 +1,49 @@
 package io.matoshri.jpa.controller;
 
-public class EmployeeController {
+import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.matoshri.jpa.model.Employee;
+import io.matoshri.jpa.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+	
+	@Autowired
+	private EmployeeService service;
+	
+	@GetMapping("/employees")
+	public ResponseEntity<List<Employee>> fetchAllEmployees() {
+		return ResponseEntity.ok(service.fetchAll());
+	}
+	
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> fetchEmployeeById(@RequestBody int empId) {
+		return ResponseEntity.of(service.fetchByEmpId(empId));
+	}
+	
+	@PostMapping("/employees")
+	public ResponseEntity<Employee> saveEmployee(@RequestBody Employee emp) {
+		return ResponseEntity.of(service.saveEmployee(emp));
+	}
+	
+	@PutMapping("/employees/{empId}")
+	public ResponseEntity<Employee> updateEmployeeById(@RequestBody int empId, @RequestBody Employee emp) {
+		return ResponseEntity.of(service.updateEmployee(empId, emp));
+	}
+	
+	@DeleteMapping("/employees/{empId}")
+	public ResponseEntity<Map<String, Boolean>> deleteEmployeeById(@RequestBody int empId) {
+		return ResponseEntity.ok(service.deleteEmployee(empId));
+	}
 }
