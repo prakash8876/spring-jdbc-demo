7849790945d4957b8318450e3b9d0c9a58929d78
prakash8876 <42605640+prakash8876@users.noreply.github.com>
2022-08-18 17:36:37 +0530

    Market project added with PPT

---
+++ market/src/test/java/io/matoshri/market/MarketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.matoshri.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
